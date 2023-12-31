9fdda42ff67c5ee62c336e04bdbb153348610a22
alper <alperensurucu.eng@gmail.com>
2023-12-31 20:12:12 +0300

    Happy Christmas

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddControllersWithViews(); //added service which is MVC 
+var app = builder.Build(); // this service is building 
 
-app.MapGet("/", () => "Hello World!");
+// MVS  ; added servce of this
+// Rest API ; we will add  different services for rest api
+// Razor pages  ; we eill add different service for razor pages
 
-app.Run();
+//app.MapGet("/", () => "Hello World!");
+//app.MapGet("/yaz", () => "alperen");    --> alperen yazacaktýr /yaz çaðýrýldýðý zaman 
+
+//app.MapDefaultControllerRoute();   //  yani þu þemayý kullan demek. --> {Controller=Home}/{Action=Index}/Ýd?
+
+app.MapControllerRoute(
+    name:"Defaullt",                           // elle yazmak istersek de  pattern i falan manuel yazýyoruz.
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.Run(); // and running 
