7384b4403b21153cadf9b775a80842f9baf26098
alper <alperensurucu.eng@gmail.com>
2024-01-12 01:51:40 +0300

    I have completed my project, including form validation operations with error messages :)

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace MeetingApp.Models
 {
     public class UserInfo 
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-        public bool? WillAttend { get; set; }
+        [Required]
+        public int Id { get; set; }  //nul bir değer geldği zaman REQUİRED  bu alan zorunludur mesajı verecek . 
+        [Required(ErrorMessage = "Ad alanı zorunlu!")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Telefon alanı zorunlu!")]
+        public string? Phone { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Katılım durumunuzu belirtiniz.")]
+        public bool? WillAttend { get; set; }  //true ,false, null (bool türünün default u false tur .)
 
     }
 }
